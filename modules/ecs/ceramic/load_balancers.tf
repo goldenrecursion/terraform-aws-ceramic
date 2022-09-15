3cc44221eecd1a1979fa21225da1255b3d0ca4ad
rhys <rhys.davies901@gmail.com>
2022-09-15 13:25:31 -0700

    removing access logs to get a deploy out

--- modules/ecs/ceramic/load_balancers.tf
+++ modules/ecs/ceramic/load_balancers.tf
@@ -11,10 +11,6 @@ module "alb" {
   security_groups = [module.load_balancer_security_group.security_group_id]
   idle_timeout    = 3600
 
-  access_logs = {
-    bucket = module.s3_alb.s3_bucket_id
-  }
-
   target_groups = [
     {
       name_prefix      = "cera-"
